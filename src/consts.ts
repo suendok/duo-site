546b81666e0f7dd097b2aefd7ead8121fc593d7d
Duo Sun <sunduopersonal@outlook.com>
2026-03-22 22:15:55 +0000

    major update

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+export const SITE = {
+  name: 'Duo Sun',
+  title: 'Duo Sun',
+  description:
+    'Personal website of Duo Sun, an economics student in the UK focused on finance, technology, research, and writing.',
+  email: 'sunduowork@outlook.com',
+  location: 'United Kingdom',
+  githubUrl: 'https://github.com/suendok',
+  githubLabel: 'github.com/suendok',
+};
